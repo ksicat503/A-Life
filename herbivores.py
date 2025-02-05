73a4f97da51406c7e8f1bd77fd6f43b3e33b0c21
Hanjun Kim <155338300+hanjunkim11@users.noreply.github.com>
2025-02-05 19:54:30 +0000

    created parent class 'organisms' with subclasses 'herbivores' and 'carnivores'

--- herbivores.py
+++ herbivores.py
@@ -1,86 +1,34 @@
 import random
-#import pygame
+from organisms import Organisms
 
-class Herbivores:
+class Herbivores(Organisms):
     def __init__(self, x_pos, y_pos):
-        # all values below should be adjusted post test simulations
-        self.x_pos = x_pos
-        self.y_pos = y_pos
-        self.age = 0
-        self.days_since_fed = 0
-        self.energy_level = 5
-        self.life_expectancy = 20
-        self.offspring_chance = 0.2
-        self.can_reproduce = False
-        if self.age >= 5 and self.age <= 15:
-            self.can_reproduce = True
-        self.is_alive = True
+        super().__init__(x_pos, y_pos)
+        self.maturation_age = 5
+        self.offspring_chance = 0.1
     
     def move(self):
-        """Change position of the carnivore organism.
-        
-        Is limited by the environmental grid, and self.is_alive sets to False
-        if an instance of Carnivores object occupies same position. Instances
-        of Herbivore objects can occupy the same position. 
+        """Herbivores-speicifc movement features.
+
+        If occupying grassland, reset days_since_fed to 0.
         """
-        if not self.is_alive:
-            return
-        
-        self.x_pos = self.x_pos + random.randint(-1, 1)
-        self.y_pos = self.y_pos + random.randint(-1, 1)
+        super().move()
+        # TO-DO: reset days_since_fed to 0 when occupying grassland
 
-        # implement game events of next cycle post movement here
-        # multiple organisms of the same class can occupy same pos
-        # if new x_pos or y_pos is outside grid, return original pos
-        # if new pos is occupied by food resources, reset
-        #   self.days_since_fed to 0
-        # if new pos is occupied by a carnivore on the same turn,
-        #   set self.is_alive to False and carnivore.days_since_fed to 0
-
-    def survival_chance(self):
-        """Random survival chance. *** ADD DISASTER-SURVIVAL CHANCE HERE???***
-        
-        Age of the organism is incremented, then calculates survival
-            chance by:
-
-            (life expectancy - current age) / (life expectancy)
-            
-        Days since last feeding day is incremented, then calculates survival
-            chance by:
-
-            (energy level - days since fed) / (energy level)
-
-            if either values are below a random float between 0 and 1, set
-            self.is_alive to False
-        """
-        if not self.is_alive:
-            return
-        
-        self.age += 1
-        chance_by_age = (self.life_expectancy - self.age) \
-            / self.life_expectancy
-        
-        self.days_since_fed += 1
-        chance_by_energy = (self.energy_level - self.days_since_fed) \
-            / self.energy_level
-
-        if chance_by_energy or chance_by_age < random.random():
-            self.is_alive = False
-        
-        '''
-        *** (POTENTIALLY) IMPLEMENT DISASTER PARAMETERS HERE,
-        IMPORT DISASTER PAREMETERS FROM 'ENVIRONMENT' CLASS? ***
-        '''
-
-    def reproduction_chance(self):
+    def reproduce(self):
         """Random chance of offspring reproduction.
         
-        If organism age is between 5 and 15 inclusive, there is a
-        self.offspring_chance probability that a new Herbivores instance of
-        object created in the same position.
+        If Herbivore age is higher than maturation_age, create a new instance
+        of Herbivore object next to it.
         """
         if not self.is_alive:
             return
         
-        if self.can_reproduce and self.offspring_chance > random.random():
-            Herbivores(self.x_pos, self.y_pos)
+        if self.age > self.maturation_age and \
+            self.offspring_chance > random.random():
+            new_x_pos = self.x_pos + self.org_width
+            if new_x_pos > self.window_w:
+                new_x_pos = self.x_pos - self.org_width
+            # TO-DO: if new organism instance location is already occupied,
+            # return without reproducing
+            Herbivores(new_x_pos, self.y_pos)
